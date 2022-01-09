d1f1f24cc740ecd03e5f2547629301d3a0ec73bd
Ross Cooper <ross@craftaweb.co.uk>
2022-01-09 10:54:51 +0000

    Added tailwind

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Login = () => {
 	return (
-		<div>
-			<div>Login</div>
+		<div className="flex items-center w-screen h-screen justify-items-center">
+			<div className="shadow m-auto max-w-full w-96">
+				<h1 className="text-xl p-4 text-center font-semibold">Login</h1>
+			</div>
 		</div>
 	);
 }
 
-export default Login;
+export default Login; 
