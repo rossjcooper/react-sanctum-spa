df177a41ac7f694d12c1671576d65b1bea985255
Ross Cooper <ross@craftaweb.co.uk>
2022-01-09 10:42:53 +0000

    Added react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './pages/Login';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+       <Routes>
+           <Route path="/" element={<App/>} />
+           <Route path="/login" element={<Login/>} />
+       </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
